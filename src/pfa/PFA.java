9e184d9b4110f1d0cb9dbfed92ac744d8a6534d5
Compaq <Compaq@BAHA-PC>
2014-04-02 03:58:26 +0100

    new commit

--- src/pfa/PFA.java
+++ src/pfa/PFA.java
@@ -5,18 +5,39 @@
  */
 
 package pfa;
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
 
 /**
  *
  * @author Compaq
  */
-public class PFA {
+public class PFA extends SingleFrameApplication{
+/**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new GererView(this));
+    }
 
     /**
-     * @param args the command line arguments
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
      */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of Gerer_Application
+     */
+    public static PFA getApplication() {
+        return Application.getInstance(PFA.class);
+    }
     public static void main(String[] args) {
         // TODO code application logic here
+        launch(PFA.class, args);
     }
     
 }
